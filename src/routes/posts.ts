f9e823b6fb72e56ec2ef46389196193bb0f5e0b0
rakesh <rakeshhotker@gmail.com>
2022-03-27 21:58:45 +0530

    commentsdone
    Signed-off-by: rakesh <rakeshhotker@gmail.com>

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,4 +1,5 @@
 import { Router,Request, Response } from "express"
+import Comment from "../entities/Comment";
 import Post from "../entities/Post";
 import Sub from "../entities/Sub";
 import auth from '../middleware/auth'
@@ -32,15 +33,33 @@ const getPosts=async (_:Request,res:Response)=>{
 const getPost=async(req:Request,res:Response)=>{
     const {identifier,slug}=req.params
     try {
-        const post=await Post.findOneOrFail({identifier,slug})
+        const post=await Post.findOneOrFail({identifier,slug},{relations:['sub','comments']})
         return res.json(post)
     } catch (error) {
         console.log(error)
         return res.status(404).json({error:'Post not found'})
     }
 }
+const commentOnPost=async(req:Request,res:Response)=>{
+    const {identifier,slug}=req.params
+    const body=req.body.body
+    try {
+        const post=await Post.findOneOrFail({identifier,slug})
+        const comment=new Comment({
+            body,
+            user:res.locals.user,
+            post
+        })
+        await comment.save()
+        return res.json(comment)
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({error:'Post not found'})
+    }
+}
 const router=Router()
 router.post('/',auth,createPost)
 router.get('/',getPosts);
-router.get('/:identifier/:slug',getPost)
+router.get('/:identifier/:slug',auth,getPost)
+router.post('/:identifier/:slug/comments',auth,commentOnPost)
 export default router
